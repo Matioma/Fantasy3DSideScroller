bd1b2f919e9080d2679fc9139eddf7ee3c8e1427
Matioma <Artiom.Matievschi@gmail.com>
2021-03-10 08:35:26 +0100

    Addd posibility to kill enemy

---
+++ Fantasy3DSideScroller/Assets/Scripts/EnemyStats.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EnemyStats : MonoBehaviour
+{
+    [SerializeField]
+    float health=1;
+
+    public UnityEvent onDeath;
+    public UnityEvent onHit;
+
+    private void Start()
+    {
+        onDeath.AddListener(Die);
+    }
+
+
+    public float Health{ get {
+            return health;
+        }
+        set {
+            Debug.Log(value);
+            health = value;
+            if (health < 0) {
+                health = 0;
+                onDeath?.Invoke();     
+            }
+        } 
+    }
+
+
+    public void Die() {
+        Destroy(gameObject);
+    }
+
+    public void Hit(float damage) {
+        Health -= damage;
+        onHit?.Invoke();
+    }
+}
