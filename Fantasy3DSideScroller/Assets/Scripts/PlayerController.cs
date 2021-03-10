931b27051af463338289f2ede682c578fa67c045
Matioma <Artiom.Matievschi@gmail.com>
2021-03-10 07:33:51 +0100

    Move default aseests

---
+++ Fantasy3DSideScroller/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMovement))]
+public class PlayerController : MonoBehaviour
+{
+    PlayerMovement playerMovement;
+    private void Awake()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+
+
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.D)) {
+            playerMovement.MoveForward();
+        }
+
+        if (Input.GetKey(KeyCode.A)) {
+            playerMovement.MoveBackward();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            playerMovement.Jump();
+        }
+    }
+}
---
+++ Fantasy3DSideScroller/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+    [Range(0, 10)]
+    [SerializeField]
+    float speed;
+
+    [Range(200, 1000)]
+    [SerializeField]
+    float jumpForce;
+
+
+    Rigidbody rigidbody;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+
+    public void MoveForward() {
+        Vector3 velocityY = new Vector3(0, rigidbody.velocity.y,0);
+        Vector3 velocityXZplane = (new Vector3(rigidbody.velocity.x, 0, rigidbody.velocity.z) + transform.forward * speed).normalized * speed;
+
+        rigidbody.velocity = velocityXZplane+ velocityY;
+    }
+
+
+    public void MoveBackward() {
+        Vector3 velocityY = new Vector3(0, rigidbody.velocity.y, 0);
+        Vector3 velocityXZplane = (new Vector3(rigidbody.velocity.x, 0, rigidbody.velocity.z) + -transform.forward * speed).normalized * speed;
+
+        rigidbody.velocity = velocityXZplane + velocityY;
+    }
+
+    public void Jump() {
+        rigidbody.AddForce(transform.up*jumpForce);        
+    }
+
+}
