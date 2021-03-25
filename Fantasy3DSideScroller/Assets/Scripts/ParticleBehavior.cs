fb7e7e5b7ea6e75db3acc019223d3e4d3baaac07
Matioma <Artiom.Matievschi@gmail.com>
2021-03-25 08:32:12 +0100

    Pulling Object dropped from kill

---
+++ Fantasy3DSideScroller/Assets/Scripts/ParticleBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleBehavior : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player;
+
+    [SerializeField]
+    float ClosingDistanceForce = 100;
+
+    [SerializeField]
+    float pullDistance = 15;
+
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>().gameObject;
+    }
+
+    void Update()
+    {
+        PullToPlayer();
+    }
+
+    void PullToPlayer()
+    {
+        if ((player.transform.position - transform.position).sqrMagnitude >= pullDistance*pullDistance) return;
+
+        Vector3 targetVector = (player.transform.position - transform.position).normalized;
+        GetComponent<Rigidbody>().velocity+=targetVector*1;
+    }
+}
