75a95b71d3c4cb6b93f937da2d465cf95a71c253
Matioma <Artiom.Matievschi@gmail.com>
2021-03-25 07:50:34 +0100

    Add Particles Spawning

---
+++ Fantasy3DSideScroller/Assets/Scripts/ParticleSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSpawner : MonoBehaviour
+{
+    [SerializeField]
+    GameObject collectiblePrefab;
+
+    [SerializeField]
+    float explosionForse;
+    [Range(0,5)]
+    [SerializeField]
+    float spawnRange;
+
+
+    public void SpawnParticles(int count) {
+        for (int i = 0; i < count; i++) {
+            SpawnParticle();
+        }
+    }
+
+    void SpawnParticle()
+    {
+        GameObject obj = Instantiate(collectiblePrefab);
+        Vector3 randomOffsetVector = new Vector3(0, Random.Range(0, spawnRange), Random.Range(-spawnRange, spawnRange));
+
+
+        obj.transform.position =transform.position;
+        Rigidbody rigidbody = obj.GetComponent<Rigidbody>();
+        rigidbody?.AddExplosionForce(explosionForse, transform.position-new Vector3(0,-1),spawnRange);
+    }
+
+}
